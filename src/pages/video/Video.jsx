79e09caf828f927661a83074d60f91dd57084381
hakkanoodles <47018rohit@gmail.com>
2023-09-21 17:59:39 +0530

    video player fixing

--- src/pages/video/Video.jsx
+++ src/pages/video/Video.jsx
@@ -1,22 +1,46 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import ReactPlayer from 'react-player'
+import { Box, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import YouTube from 'react-youtube'
 import { useSelector } from 'react-redux'
+import styles from './video.module.css'
 
 const Video = () => {
-
+    const [frameSize, setFrameSize] = useState({ width: 1080, height: 720 })
     const data = useSelector(state => state.video)
+
+    const handleReady =(e)=>{
+        console.log(e.target.getVideoEmbedCode())
+    }
+
     return (
-        <Box className="videoWrapper" sx={{
-            width: '100%',
-            backgroundColor: '#aaa',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}
-        >
-            <section>
-                <ReactPlayer url={`https://www.youtube.com/watch?v=${data.videoId}`} playing={true} controls={true} width={1080} height={720} />
+        <Box className={styles.videoWrapper} >
+            <section className='video'>
+                <YouTube
+                    opts={{
+                        width: frameSize.width,
+                        height: frameSize.height
+                    }}
+                    videoId={data.videoId}
+                    title={data.title}
+                    onReady={handleReady}
+                />
+                {frameSize.width > 0 && frameSize.height > 0 && (
+                    <p>
+                        Video Frame Dimensions: {frameSize.width}x{frameSize.height}
+                    </p>
+                )}
+                <Typography sx={{ fontSize: '25px', fontWeight: '600', marginTop: '1.5rem' }}>
+                    {data.title}
+                </Typography>
+                <Typography sx={{ fontSize: '18px', fontWeight: '600' }}>
+                    {data.channelTitle}
+                </Typography>
+                <Typography sx={{ fontSize: '14px', fontWeight: '200' }}>
+                    {data.description}
+                </Typography>
+            </section>
+            <section className={styles.suggestedVideos}>
+
             </section>
         </Box>
     )
